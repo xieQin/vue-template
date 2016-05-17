b3b5ec2ca4a74e73e17c2163ea169484a8a48821
Miller Ren <ren12345@126.com>
2016-05-17 16:47:04 +0800

    rooter async components

--- src/asyncs.js
+++ src/asyncs.js
@@ -1,9 +1,9 @@
 export default function (Vue) {
   Vue.component('Lottery', function (resolve, reject) {
-    require(['./components/Lottery.vue'], resolve)
+    require(['./components/Lottery/index.vue'], resolve)
   })
 
   Vue.component('Room', function (resolve, reject) {
-    require(['./components/Room.vue'], resolve)
+    require(['./components/Room/index.vue'], resolve)
   })
 }
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,17 @@
 import Hello from './components/Hello/index.vue'
-import Temp from './components/Template/index.vue'
 
+/* eslint-disable */
 export default function (router) {
   router.map({
     '/': {
       name: 'home',
       component: Hello
     },
-    '/data': {
-      name: 'temp',
-      component: Temp
+    '/room': {
+      name: 'room',
+      component: function(resolve){
+        require(['./components/Room/index.vue'], resolve)
+      }
     }
   })
 
